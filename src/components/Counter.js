558d0c764d4b5c0d10d92040c8ded99389abb98b
sunil singh parihar <sunilsinghparihar5203@gmail.com>
2023-04-05 00:46:52 +0530

    Integrating Redux with react

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,12 +1,35 @@
-import classes from './Counter.module.css';
+import classes from "./Counter.module.css";
+import { useSelector, useDispatch } from "react-redux";
 
 const Counter = () => {
+  const counter = useSelector((state) => state.counter);
+  const dispatch = useDispatch();
   const toggleCounterHandler = () => {};
 
+  const incrementHandler = () => {
+    dispatch({ type: "increment" });
+  };
+  const decrementHandler = () => {
+    dispatch({ type: "decrement" });
+  };
+
+  const incrementBy2Handler = () => {
+    dispatch({ type: "INCREMENTBY2" });
+  };
+
+  const decrementBy2Handler = () => {
+    dispatch({ type: "DECREMENTBY2" });
+  };
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>-- COUNTER VALUE --</div>
+      <div className={classes.value}>{counter}</div>
+      <div>
+        <button onClick={incrementHandler}>Increment</button>
+        <button onClick={decrementHandler}>Decrement</button>
+        <button onClick={incrementBy2Handler}>Increment BY 2</button>
+        <button onClick={decrementBy2Handler}>Decrement BY 2</button>
+      </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
